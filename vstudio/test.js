e3240363991c333ab1854d0ff65cb83e6eeadafb
tanaporn.suea <tanaporn.sues@ku.th>
2021-01-11 15:31:15 +0700

    .js

---
+++ vstudio/test.js
@@ -0,0 +1,12 @@
+function isPrimeNumber(n) {
+    if (n < 2) {
+        return false;
+    }
+    for (var num = 2; num < n; num++) {
+        if (n % num === 0) {
+            return false;
+        }
+    }
+    return true;
+}
+console.log(isPrimeNumber(2));
